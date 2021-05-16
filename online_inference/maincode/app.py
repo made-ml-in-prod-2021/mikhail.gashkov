a4f8f5c5078ac57e542229f5f79c723ca13f614f
MikhailYeka <mihael.gashkov@gmail.com>
2021-05-16 20:02:03 +0500

    Added FastAPI app, tests

--- online_inference/maincode/app.py
+++ online_inference/maincode/app.py
@@ -1,11 +1,17 @@
 import logging
 import os
+from pathlib import Path
+from typing import List
 
-from fastapi import FastAPI
+import uvicorn as uvicorn
+from fastapi import FastAPI, HTTPException
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
 import numpy as np
 import pandas as pd
 
-
+from .backend import make_predictions
+from .data_model import HealthModel, HealthResponse
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
@@ -14,10 +20,25 @@ GREETING_MESSAGE = 'Hello, this is prediction health service. Go to /predict end
 
 
 @app.get('/')
-def read_root():
+def read_root_handler():
     return {'text': GREETING_MESSAGE}
 
 
-@app.get('/predict')
-def predict_handler():
-    pass
+@app.exception_handler(HTTPException)
+def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+
+@app.exception_handler(RequestValidationError)
+def validation_exception_handler(request, exc):
+    return PlainTextResponse(str(exc), status_code=400)
+
+
+@app.post('/predict', response_model=List[HealthResponse])
+def predict_handler(request: HealthModel):
+    logger.info('Making predictions')
+    return make_predictions(request.data, request.features)
+
+
+if __name__ == "__main__":
+    uvicorn.run("app", host="0.0.0.0", port=os.getenv("PORT", 8000))
