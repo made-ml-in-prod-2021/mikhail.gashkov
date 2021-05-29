c493cbfbb1a82d65bcd2db0b370cd02178742e03
MikhailYeka <mihael.gashkov@gmail.com>
2021-05-29 16:29:34 +0500

    Deleted hw1 and hw2 from hw3

--- ml_project/maincode/model_pipeline/custom_exceptions.py
+++
@@ -1,8 +0,0 @@
-class NotExistingModelException(Exception):
-    def __repr__(self):
-        return 'Please, choose correct model'
-
-
-class ModelNotCreatedException(Exception):
-    def __repr__(self):
-        return 'Please, create a model first'
