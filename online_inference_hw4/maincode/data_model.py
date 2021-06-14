7ea00904fe6172f4f04f6d6203cb4482af6cf021
MikhailYeka <mihael.gashkov@gmail.com>
2021-06-14 13:07:12 +0500

    Assignment 2

---
+++ online_inference_hw4/maincode/data_model.py
@@ -0,0 +1,32 @@
+from typing import List, Union
+
+from pydantic import BaseModel, conlist, validator
+
+CATEGORICAL_FEATURES_LIST = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
+NUMERICAL_FEATURES_LIST = [
+    'sex',
+    'cp',
+    'fbs',
+    'restecg',
+    'exang',
+    'slope',
+    'ca',
+    'thal',
+]
+INPUT_FEATURES_LIST = [*CATEGORICAL_FEATURES_LIST, *NUMERICAL_FEATURES_LIST]
+WRONG_FEATURES_MESSAGE = 'You have loaded wrong data'
+
+
+class HealthModel(BaseModel):
+    data: List[conlist(Union[float, str], min_items=1)]
+    features: List[str]
+
+    @validator('features')
+    def validate_features(cls, features):
+        if features != INPUT_FEATURES_LIST:
+            raise ValueError(WRONG_FEATURES_MESSAGE)
+        return features
+
+
+class HealthResponse(BaseModel):
+    target: List[int]
